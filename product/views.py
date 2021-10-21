b9e76be5518d1d19bb22c6acdda8430a8b9a2c32
derarsat <74964227+derarsat@users.noreply.github.com>
2021-10-21 16:48:19 +0300

    .

--- product/views.py
+++ product/views.py
@@ -517,6 +517,5 @@ def rawad(request):
             products = paginator.page(paginator.num_pages)
 
     for product in products:
-        product.weight_value = (float(product.quantity * product.quantity_type.value) + float(
-            product.extra_quantity)) / float(product.weight_value)
+        product.weight_value = (float(product.quantity * product.quantity_type.value) + float(product.extra_quantity)) * float(product.weight_value)
     return render(request, "product/rawad.html", {"products": products})
