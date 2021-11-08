1a51dacee7d8f504d2b32c872a02a961f686bbd3
derarsat <74964227+derarsat@users.noreply.github.com>
2021-11-08 12:41:04 +0200

    Update settings.py

--- turbo/settings.py
+++ turbo/settings.py
@@ -99,7 +99,7 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'),)
 
