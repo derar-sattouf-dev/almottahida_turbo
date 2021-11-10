a2e5ae0eea1372f46d2457264d33c4dab3a76435
derarsat <74964227+derarsat@users.noreply.github.com>
2021-11-10 05:09:47 +0200

    Update settings.py

--- turbo/settings.py
+++ turbo/settings.py
@@ -75,7 +75,6 @@ DATABASES = {
         'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
-            'autocommit': True,
         },
     }
     # 'default': {
