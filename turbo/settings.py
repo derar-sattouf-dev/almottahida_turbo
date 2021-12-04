1469978544115f19b52a712acfc0a38cb9003ced
Derar Sattouf <derar.satouf@toothpickapp.com>
2021-12-04 14:16:43 +0200

    test

--- turbo/settings.py
+++ turbo/settings.py
@@ -64,6 +64,10 @@ WSGI_APPLICATION = 'turbo.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    #     'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     # 'default': {
     #     'ENGINE': 'mysql.connector.django',
     #     'NAME': 't',
